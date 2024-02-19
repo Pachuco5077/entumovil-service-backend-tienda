a4d480e406f520cb5381454e04fa7b35eabec956
Pachuco <51896548+Pachuco5077@users.noreply.github.com>
2024-02-19 08:04:30 -0500

    Update EntumovilServiceBackendTiendaApplicationTests.java

--- src/test/java/cu/entumovil/ecommerce/EntumovilServiceBackendTiendaApplicationTests.java
+++ src/test/java/cu/entumovil/ecommerce/EntumovilServiceBackendTiendaApplicationTests.java
@@ -1,12 +1,12 @@
 package cu.entumovil.ecommerce;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EntumovilServiceBackendTiendaApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
